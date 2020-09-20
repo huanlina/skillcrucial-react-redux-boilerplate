daa54f0d38125a46281ced25132f1d27b7a6587e
mac <mac@macdeMacBook-Air.local>
2020-09-20 16:23:39 +0800

    updated

--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ import bodyParser from 'body-parser'
 import sockjs from 'sockjs'
 import { renderToStaticNodeStream } from 'react-dom/server'
 import React from 'react'
-
+import axios from 'axios'
 import cookieParser from 'cookie-parser'
 import config from './config'
 import Html from '../client/html'
+
+const { writeFile } = require("fs").promises
 
 const Root = () => ''
 
@@ -31,15 +33,42 @@ let connections = []
 const port = process.env.PORT || 8090
 const server = express()
 
+const setHeaders = (req, res, next) => {
+  res.set('x-skillcrucial-user', '2b2268af-407f-4d42-af7f-f4f1d1eebe69')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER') 
+  next()
+}
+
 const middleware = [
   cors(),
   express.static(path.resolve(__dirname, '../dist/assets')),
   bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }),
   bodyParser.json({ limit: '50mb', extended: true }),
-  cookieParser()
+  cookieParser(),
+  setHeaders
 ]
-
 middleware.forEach((it) => server.use(it))
+
+server.get('/api/v1/users/', async (req, res) => {  
+  const users = await axios('https://jsonplaceholder.typicode.com/users').then(result => JSON.stringify(result.data))  
+  writeFile(`${__dirname}/users.json`, users, { encoding: "utf8" })
+  res.json(JSON.parse(users))  
+})  
+
+server.post('/api/v1/users',  (req, res) => {  
+  res.json( { id: '', ...req.body })  
+}) 
+
+// server.get('/api/v1/users/take/:number', async (req, res) => {  
+//   const { number } = req.params  
+//   const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')  
+//   res.json(users.slice(0, +number))  
+// })  
+
+// server.get('/api/v1/users/:name', (req, res) => {  
+//   const { name } = req.params  
+//   res.json({ name })  
+// })  
 
 server.use('/api/', (req, res) => {
   res.status(404)
